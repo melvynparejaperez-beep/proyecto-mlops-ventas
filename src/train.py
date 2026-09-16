52d8b763c3c689dba61bf6932ec8c71e42b749a9
Danny <melvynparejaperez@gmail.com>
2026-09-16 16:37:43 -0500

    ver10

--- src/train.py
+++ src/train.py
@@ -1,6 +1,9 @@
+import os
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
+
+os.makedirs("models", exist_ok=True)
 
 datos = pd.read_csv("data/ventas.csv")
 
@@ -8,9 +11,8 @@ X = datos[["dia"]]
 y = datos["ventas"]
 
 modelo = LinearRegression()
-
 modelo.fit(X, y)
 
 joblib.dump(modelo, "models/modelo.pkl")
 
-print("Modelo guardado")
+print("Modelo guardado") 
